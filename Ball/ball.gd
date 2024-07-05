205ebec78d677db6ac8ad20dd9eeab4f5900b24c
SetHet <36393893+SetHet@users.noreply.github.com>
2024-07-05 04:36:38 -0400

    collision y classname

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+class_name Ball
 
 var speed = 0
 var maxSpeed = 500
--- Levels/level.gd
+++ Levels/level.gd
@@ -22,14 +22,16 @@ func RestartGame(timeout = 0):
 
 
 func _on_area_2d_body_entered(body):
-	pointsOponente += 1
-	$Marcador.ActualizarMarcador(pointsPlayer, pointsOponente)
-	await get_tree().create_timer(timeout_gol).timeout
-	RestartGame(timeout_gol)
+	if body is Ball:
+		pointsOponente += 1
+		$Marcador.ActualizarMarcador(pointsPlayer, pointsOponente)
+		await get_tree().create_timer(timeout_gol).timeout
+		RestartGame(timeout_gol)
 
 
 func _on_area_2d_body_entered_derecha(body):
-	pointsPlayer += 1
-	$Marcador.ActualizarMarcador(pointsPlayer, pointsOponente)
-	await get_tree().create_timer(timeout_gol).timeout
-	RestartGame(timeout_gol)
+	if body is Ball:
+		pointsPlayer += 1
+		$Marcador.ActualizarMarcador(pointsPlayer, pointsOponente)
+		await get_tree().create_timer(timeout_gol).timeout
+		RestartGame(timeout_gol)
